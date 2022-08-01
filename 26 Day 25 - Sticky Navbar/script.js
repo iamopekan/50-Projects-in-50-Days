72f0448109211afa174f6a4a614a108f45cae1d6
Ope Ajayi <105161691+Iamopekan@users.noreply.github.com>
2022-08-01 20:56:23 +0100

    Add files via upload

---
+++ 26 Day 25 - Sticky Navbar/script.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector ('.nav')
+window.addEventListener('scroll', fixedNav)
+
+function fixedNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
